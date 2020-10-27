99dbc9fd767b405ddc5c3ae0f78cd29758e8e15e
unibreakfast <unibreakfast1@gmail.com>
2020-10-27 20:56:28 +0200

    upgrade_require_to_update

---
+++ index.js
@@ -0,0 +1,33 @@
+let done = false
+
+module.exports = function upgrade_require_to_update() {
+  if (done) return
+
+  const originalRequire = require
+  const reloadingWrappers = {}
+
+  require = function require(module, watchToReload) {
+    if (watchToReload) {
+      if (module in reloadingWrappers) {
+        return reloadingWrappers[module]
+      } else {
+        originalRequire("fs").watch(
+          originalRequire("path").resolve(module),
+          () => delete require.cache[require.resolve(module)]
+        )
+
+        reloadingWrappers[module] = function (...args) {
+          return originalRequire(module).apply(this, args)
+        }
+
+        return Object.assign(reloadingWrappers[module], originalRequire(module))
+      }
+    }
+
+    return originalRequire(module)
+  }
+
+  Object.assign(require, originalRequire)
+
+  done = true
+}
